3c488fb3667a187bdb3336b1fe1830127af50cc7
mtaariqkagee <30648267+mtaariqkagee@users.noreply.github.com>
2019-11-05 08:49:25 +0200

    Update ml_viz_utils.py
    Added cv folds count

--- ml_viz_utils.py
+++ ml_viz_utils.py
@@ -1,19 +1,3 @@
-def display_plot(cv_scores, cv_scores_std):
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.plot(alpha_space, cv_scores)
-
-    std_error = cv_scores_std / np.sqrt(10)
-
-    ax.fill_between(alpha_space, cv_scores + std_error, cv_scores - std_error, alpha=0.2)
-    ax.set_ylabel('CV Score +/- Std Error')
-    ax.set_xlabel('Alpha')
-    ax.axhline(np.max(cv_scores), linestyle='--', color='.5')
-    ax.set_xlim([alpha_space[0], alpha_space[-1]])
-    ax.set_xscale('log')
-    plt.show()
-    
-  
 # Import necessary modules
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score
@@ -33,7 +17,7 @@ for alpha in alpha_space:
     ridge.alpha = alpha
     
     # Perform 10-fold CV: ridge_cv_scores
-    ridge_cv_scores = cross_val_score(ridge, X, y)
+    ridge_cv_scores = cross_val_score(ridge, X, y, cv=10)
     
     # Append the mean of ridge_cv_scores to ridge_scores
     ridge_scores.append(np.mean(ridge_cv_scores))
